00be5af0d1d6af09b69faf5b3945f092cc0531b2
Bill Yuga <billyuga37@gmail.com>
2016-01-25 23:08:42 +0100

    Added a header for the arithmetic functions

---
+++ arithmetic_functions.h
@@ -0,0 +1,15 @@
+float add(int a, int b)
+{ return a + b;
+}
+
+float subt(int x, int y)
+{return x-y;
+}
+
+float mult(int r, int s)
+{return r*s;
+}
+
+float div(int p, int q)
+{return p/q;
+}
--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include "arithmetic_functions.h"
 
 int main()
 {
 int num1, num2, answer;
+
+printf("Enter two numbers");
 
 return 0;
 }
