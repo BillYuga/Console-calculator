7a5a6e32daa8c592f9fea45397f55e2cc2548a67
BillYuga <billyuga37@gmail.com>
2017-02-11 12:56:39 -0600

    Update main.c

--- main.c
+++ main.c
@@ -3,9 +3,22 @@
 
 int main()
 {
-int num1, num2, answer;
+float operand1, operand2;
+float result;
+char operator;
+  
 
-printf("Enter two numbers");
-
+printf("Enter expression (operand operator operand): ");
+scanf("%f %d %f",&operand1, &operator, &operand2);
+switch(operator){
+  case '+': result = add(operand1, operand2);
+            break;
+  case '-': result = subt(operand1, operand2);
+            break;
+  case '*': result = mult(operand1, operand2);
+            break;
+  case '/': result = div(operand1, operand2);
+}
+printf("Result: %f", result);
 return 0;
 }
